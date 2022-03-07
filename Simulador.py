81dd6d745f34e9f944f031fba348bd0fa7d24858
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2022-03-06 18:21:28 -0600

    Update Simulador.py

--- Simulador.py
+++ Simulador.py
@@ -1,4 +1,14 @@
 import simpy
 import random as rand 
 
+intervalo = 10
+
+
+
+  
+
+env =  simpy.Environment()
+res = simpy.Resource(env, capacity = 1)
+
 RAM = simpy.Container(env, init=100, capacity=100)
+
