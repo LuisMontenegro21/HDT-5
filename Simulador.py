f8640b2ffd8af6e44742a06f56db3bb1c0aa99ed
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2022-03-08 21:31:09 -0600

    Update Simulador.py

--- Simulador.py
+++ Simulador.py
@@ -3,17 +3,18 @@ import random as rand
 proces = 25
 memory = 100
 intervalo = 10
-def new (self, env)
-    while (proces != 0)
+def new (self, env):
+    while (proces != 0):
         mem = rand.randrange(1,10)
         instr = rand.randrange(1,10)
         name = "proces" + proces
+        parameters = (mem, instr, name)
+        return parameters
+    
+def ready():
+    new()
+    parameter = [new.mem, new.instr, new.name]
         
-        
-        
-
-
-  
 
 env =  simpy.Environment()
 dres = simpy.Resource(env, capacity = 1)
