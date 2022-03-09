c973ef2b25ac2c7ca94d065632e7e7b94e30c965
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2022-03-09 06:03:05 -0600

    Update Simulador.py

--- Simulador.py
+++ Simulador.py
@@ -1,19 +1,54 @@
+from inspect import Parameter
+from multiprocessing.pool import TERMINATE
+import this
 import simpy
 import random as rand
 proces = 25
 memory = 100
 intervalo = 10
+mark = {}
 def new (self, env):
     while (proces != 0):
         mem = rand.randrange(1,10)
         instr = rand.randrange(1,10)
         name = "proces" + proces
         parameters = (mem, instr, name)
+        proces =- 1
         return parameters
     
-def ready():
+def readynew(self, env, this):
     parameter = new()
-    ptr = iter (parameter)
+    return parameter
+
+def readyold(anc, env):
+    Parameter = anc
+    return Parameter
+
+def waiting(anc, env, self):
+    spec = iter(anc)
+    memp = next(spec)
+    namep = next(spec)
+    mark[namep] = spec, memp
+    
+
+def runningnew (self,env):
+    proc = readynew()
+    ex = iter(proc)
+    memp = next(ex)
+    interp = rand.randrange(1,2)
+    instrp = next(ex)
+    namep = next(ex)
+    memory =- memp
+    instrp =- 3
+    if (instrp == 0):
+        TERMINATE
+    if (interp == 1 & instrp != 0):
+        readyold(proc);
+    if (interp == 2 & instrp != 0):
+        TERMINATE
+        
+        
+        
             
 
 env =  simpy.Environment()
